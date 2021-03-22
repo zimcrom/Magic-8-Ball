d11ebc298338edbd5985bf57abd293b832087b71
Megan H <megannh6349@gmail.com>
2021-03-22 14:32:52 -0400

    added alerts, need to fix question

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
-var userName = 'Megan';
+// var userName = 'Megan';
 
-userName === true ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+// userName === true ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 
-var userQuestion = 'Is Stacey the best SSM ever?';
-
-console.log(`${userName} asked: ${userQuestion}`);
+function runEightBall(){
+    
+var userQuestion = document.getElementById('questionForm');
+alert(`You asked: ${userQuestion}`);
 
 var randomNumber = Math.floor(Math.random() * 8);
-
-var eightBall = '';
+var eightBall = "";
 
 switch (randomNumber) {
-    case 0:
-        eightBall = 'It is certain'
-        break;
-    case 1:
-        eightBall = 'It is decidedly so'
-        break;
-    case 2:
-        eightBall = 'Reply hazy, try again'
-        break;
-    case 3:
-        eightBall = 'Cannot predict now'
-        break;
-    case 4:
-        eightBall = 'Do not count on it'
-        break;
-    case 5:
-        eightBall = 'My sources say no'
-        break;
-    case 6:
-        eightBall = 'Outlook not so good'
-        break;
-    case 7:
-        eightBall = 'Signs point to yes'
-        break;
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy, try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    eightBall = "My sources say no";
+    break;
+  case 6:
+    eightBall = "Outlook not so good";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
+    break;
 }
 
-console.log(`The eight ball answered: ${eightBall}`);
+alert(`The eight ball answered: ${eightBall}`);
+
+};
+
+// Now to select the button
+var btn = document.getElementById('start');
+btn.addEventListener('click', runEightBall);
